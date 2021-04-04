f9ee916c83781a28409c35fc79397d8049fac484
Kartik Derasari <derasarikartik@gmail.com>
2021-04-05 00:17:30 +0530

    Add separate Firebase config file
    - Create separate Firebase config file
    - Add Firebase config file to gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,15 +5,10 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+//Import Firebase configurations
+import firebaseConfigData from './firebase-config';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDTQswqPtzG6cZFEiNXRlJ983QjFcVAz6o",
-    authDomain: "portfolio-89383.firebaseapp.com",
-    projectId: "portfolio-89383",
-    storageBucket: "portfolio-89383.appspot.com",
-    messagingSenderId: "558205635702",
-    appId: "1:558205635702:web:0eac07a015de22bd2fa357"
-};
+const firebaseConfig = firebaseConfigData;
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);

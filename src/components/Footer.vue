bf1cd659f0022619bd8fdb4c4baea9eca919ea31
Kartik Derasari <derasarikartik@gmail.com>
2021-04-06 15:22:27 +0530

    Make Footer more accessible

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -20,32 +20,70 @@
           md="6"
           lg="6"
         >
-          <v-btn icon :href="'mailto:' + userData.email" target="_blank">
+          <v-btn
+            icon
+            :href="'mailto:' + userData.email"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Mail"
+          >
             <v-icon>mdi-email</v-icon>
           </v-btn>
-          <v-btn icon :href="userData.Facebook" target="_blank">
+          <v-btn
+            icon
+            :href="userData.Facebook"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Facebook"
+          >
             <v-icon>mdi-facebook</v-icon>
           </v-btn>
-          <v-btn icon :href="userData.Instagram" target="_blank">
+          <v-btn
+            icon
+            :href="userData.Instagram"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Instagram"
+          >
             <v-icon>mdi-instagram</v-icon>
           </v-btn>
-          <v-btn icon :href="userData.LinkedIn" target="_blank">
+          <v-btn
+            icon
+            :href="userData.LinkedIn"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="LinkedIn"
+          >
             <v-icon>mdi-linkedin</v-icon>
           </v-btn>
-          <v-btn icon :href="userData.GitHub" target="_blank">
+          <v-btn
+            icon
+            :href="userData.GitHub"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="GitHub"
+          >
             <v-icon>mdi-github</v-icon>
           </v-btn>
-          <v-btn icon :href="userData.Twitter" target="_blank">
+          <v-btn
+            icon
+            :href="userData.Twitter"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Twitter"
+          >
             <v-icon>mdi-twitter</v-icon>
           </v-btn>
         </v-col>
         <v-col cols="12" class="text-center font-weight-medium text--secondary">
+          <v-divider class="pb-2"></v-divider>
           <strong
             >© Created by
             <a
-              class="text-decoration-none"
+              class="text-decoration-none purple--text"
               href="https://github.com/kartikderasari/"
               target="_blank"
+              rel="noreferrer noopener"
               >Kartik Derasari</a
             ></strong
           >

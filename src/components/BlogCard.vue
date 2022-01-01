3c81e331cca12010185681db1eebb463826010a3
Kartik Derasari <derasarikartik@gmail.com>
2022-01-01 20:37:31 +0530

    Add Blog components

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container fluid>
+    <v-card
+      class="d-flex flex-column"
+      elevation="2"
+      outlined
+      min-height="400"
+    >
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-img
+            class="rounded-t grey lighten-4"
+            height="22vh"
+            :src="project.projectImageURL"
+            :lazy-src="project.projectImageURL"
+            lazy
+          ></v-img>
+
+          <v-card-title class="pb-0 teal--text text--darken-2">
+            {{ project.title }}
+          </v-card-title>
+
+          <v-card-text v-if="project.brief.length < 120" class="py-0 subtitle">
+            {{ project.brief }}
+          </v-card-text>
+          <v-card-text v-else class="py-0 subtitle">
+            {{ project.brief.slice(0, 160) }}...
+          </v-card-text>
+        </v-col>
+
+        <v-col class="d-flex flex-column justify-end" cols="12">
+          <v-card-actions class="pl-4 d-flex flex-wrap">
+            <v-chip class="mb-1 mr-1" small>
+              {{ project.techStack }}
+            </v-chip>
+          </v-card-actions>
+          <v-divider class="mx-4"></v-divider>
+          <v-card-actions class="pl-4 d-flex flex-wrap pb-3">
+            <v-btn
+              class="purple white--text"
+              elevation="0"
+              small
+              target="_blank"
+              :href="project.liveURL"
+              rel="noreferrer noopener"
+              aria-label="Project URL"
+            >
+              View Project
+            </v-btn>
+            <v-btn
+              color="purple"
+              text
+              small
+              target="_blank"
+              :href="project.githubURL"
+              rel="noreferrer noopener"
+              aria-label="GitHub URL"
+            >
+              View Code
+            </v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+};
+</script>
---
+++ src/components/BlogContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container class="py-8">
+    <v-row>
+      <v-card-title
+        class="pt-5 pl-8 font-weight-medium headline text--secondary"
+      >
+        Blogs
+      </v-card-title>
+    </v-row>
+    <v-card class="py-5 px-8" color="transparent" flat>
+      <v-row v-if="loading">
+        <v-progress-circular
+          class="mx-auto my-10"
+          :size="40"
+          color="primary"
+          indeterminate
+          v-if="loading"
+        ></v-progress-circular>
+      </v-row>
+      <v-row v-if="!loading">
+        <v-col
+          cols="12"
+          sm="6"
+          md="6"
+          lg="4"
+          v-for="(project, index) in projects"
+          :key="index"
+        >
+          <BlogCard :project="project" />
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import BlogCard from "@/components/BlogCard.vue";
+
+export default {
+  data: () => ({
+    model: null,
+    projects: null,
+    loading: false,
+  }),
+  components: { BlogCard },
+  props: ["projects"],
+};
+</script>
